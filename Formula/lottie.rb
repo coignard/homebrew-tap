c921d58b96949fa7d06e1329cabda5b597f65856
René Coignard <contact@renecoignard.com>
2026-03-19 16:54:17 +0100

    update formula

--- Formula/lottie.rb
+++ Formula/lottie.rb
@@ -3,15 +3,22 @@ class Lottie < Formula
   homepage "https://lottie.rs/"
 
   on_macos do
-    url "https://github.com/coignard/lottie/releases/download/0.2.12/lottie-universal-apple-darwin.tar.gz"
-    sha256 "9dde8759eda9f61e27914e683d933459a2700a349e9d5e47e0592319299f665e"
+    on_arm do
+      url "https://github.com/coignard/lottie/releases/download/0.2.12/lottie-universal-apple-darwin.tar.gz"
+      sha256 "9dde8759eda9f61e27914e683d933459a2700a349e9d5e47e0592319299f665e"
+    end
+    on_intel do
+      url "https://github.com/coignard/lottie/releases/download/0.2.12/lottie-universal-apple-darwin.tar.gz"
+      sha256 "9dde8759eda9f61e27914e683d933459a2700a349e9d5e47e0592319299f665e"
+    end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/coignard/lottie/releases/download/0.2.12/lottie-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "c2899e60b748d97f69149bc5cf122449e2be637ed77fc06c89da7deafbb9dd05"
-    else
+    end
+    on_intel do
       url "https://github.com/coignard/lottie/releases/download/0.2.12/lottie-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "9b767dea9183a21c17cd2030b5b85b3d60cdf9e3ebff7b37ea3ac6a4556f6b87"
     end
